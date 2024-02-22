d6cf020504d56d21e2d543a394e3f2bdb2af740f
bilal <bilal@BILALJAVED>
2024-02-22 13:54:13 +0500

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={()=>alert("normal Button clicked")}>normal button</button> <br/><br/>
+    <Button onClick={()=>alert("bootstrap Button clicked")}>bootstrap button</Button>
     </div>
   );
 }
